abef731ca4402e15b91961b6303434645a21048b
like5188 <260864866@qq.com>
2019-12-11 20:45:14 +0800

    完善

--- ble/src/main/java/com/like/ble/state/State.kt
+++ ble/src/main/java/com/like/ble/state/State.kt
@@ -6,7 +6,7 @@ import com.like.ble.command.concrete.*
 import com.like.ble.model.BleResult
 
 /**
- * 蓝牙状态基类。todo 同步问题
+ * 蓝牙状态基类。
  */
 abstract class State {
     lateinit var mActivity: FragmentActivity
