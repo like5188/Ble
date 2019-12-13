4a1ec5f10f07eebca13d432b65dd278b4c39b243
like <like@adtech.com.cn>
2019-12-13 12:01:34 +0800

    用队列来管理命令

--- ble/src/main/java/com/like/ble/command/CommandInvoker.kt
+++ ble/src/main/java/com/like/ble/command/CommandInvoker.kt
@@ -4,19 +4,28 @@ import android.util.Log
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 /**
  * 命令请求者。
  */
 class CommandInvoker(private val mActivity: FragmentActivity) {
+    companion object {
+        private val TAG = CommandInvoker::class.java.simpleName
+    }
+
     private val mCommands = Channel<Command>()
 
     init {
         mActivity.lifecycleScope.launch {
             for (command in mCommands) {
-                Log.d("CommandInvoker", "开始执行：$command")
+                Log.i(TAG, "开始执行命令：$command")
                 command.execute()
+                while (!command.mIsCompleted.get()) {
+                    delay(20)
+                }
+                Log.w(TAG, "命令执行完成：$command")
             }
         }
     }
