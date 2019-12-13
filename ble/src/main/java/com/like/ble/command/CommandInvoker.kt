5a9f5c8101394a5644425da794f77ca1bbaa6b95
like <like@adtech.com.cn>
2019-12-13 14:38:15 +0800

    用队列来管理命令

--- ble/src/main/java/com/like/ble/command/CommandInvoker.kt
+++ ble/src/main/java/com/like/ble/command/CommandInvoker.kt
@@ -3,6 +3,7 @@ package com.like.ble.command
 import android.util.Log
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
+import com.like.ble.command.concrete.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -16,11 +17,13 @@ class CommandInvoker(private val mActivity: FragmentActivity) {
     }
 
     private val mCommands = Channel<Command>()
+    private var mCurCommand: Command? = null
 
     init {
         mActivity.lifecycleScope.launch {
             for (command in mCommands) {
                 Log.i(TAG, "开始执行命令：$command")
+                mCurCommand = command
                 command.execute()
                 while (!command.isCompleted()) {
                     delay(20)
@@ -31,6 +34,43 @@ class CommandInvoker(private val mActivity: FragmentActivity) {
     }
 
     fun addCommand(command: Command) {
+        val curCommand = mCurCommand
+        // 判断需要抛弃
+        if (curCommand != null) {
+            if (isSameCommand(command, curCommand)) {
+                return
+            }
+        }
+        // 判断排队
+
+        // 判断立即执行
+
+        sendCommand(command)
+    }
+
+    /**
+     * 判断是否是同一命令。
+     *
+     * 其中[InitCommand]、[StartAdvertisingCommand]、[StopAdvertisingCommand]、[StartScanCommand]、[StopScanCommand]、[CloseCommand]是同一类型，则判断为同一命令
+     * 其中[ConnectCommand]、[DisconnectCommand]、[ReadCharacteristicCommand]、[WriteCharacteristicCommand]、[SetMtuCommand]是同一类型，并且内容相同，才判断为同一命令
+     */
+    private fun isSameCommand(command1: Command, command2: Command): Boolean {
+        if (command1::class.java == command2::class.java) {
+            when (command1) {
+                is ConnectCommand, is DisconnectCommand, is ReadCharacteristicCommand, is WriteCharacteristicCommand, is SetMtuCommand -> {
+                    if (command1 == command2) {// 必须比较内容
+                        return true
+                    }
+                }
+                else -> {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun sendCommand(command: Command) {
         mActivity.lifecycleScope.launch {
             mCommands.send(command)
         }
