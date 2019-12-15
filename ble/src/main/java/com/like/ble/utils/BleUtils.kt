b03fc064ab756390f38dd7b31d5125191ed46dbe
like5188 <260864866@qq.com>
2019-12-15 14:20:35 +0800

    完善

--- ble/src/main/java/com/like/ble/utils/BleUtils.kt
+++ ble/src/main/java/com/like/ble/utils/BleUtils.kt
@@ -27,25 +27,14 @@ fun Context.isSupportBluetooth(): Boolean = packageManager.hasSystemFeature(Pack
 /**
  * 查找远程设备的特征，并开启通知，以便触发onCharacteristicChanged()方法
  */
-internal fun BluetoothGatt.findCharacteristic(
-    characteristicUuidString: String,
-    enableNotification: Boolean = false
-): BluetoothGattCharacteristic? {
-    // 开始查找特征
-    val characteristic = services
+internal fun BluetoothGatt.findCharacteristic(characteristicUuidString: String): BluetoothGattCharacteristic? =
+    services
         ?.flatMap {
             it.characteristics
         }
         ?.firstOrNull {
             it.uuid.toString() == characteristicUuidString
         }
-
-    if (enableNotification && characteristic != null) {
-        // 接受Characteristic被写的通知,收到蓝牙模块的数据后会触发onCharacteristicChanged()
-        setCharacteristicNotification(characteristic, true)
-    }
-    return characteristic
-}
 
 /**
  * 把 ByteArray 按照指定的 chunkSize 进行分批处理
