41da9cd45e6039e15a4d5f1832dd0309b4cb1d53
like <like@adtech.com.cn>
2019-12-31 14:39:15 +0800

    完善

--- ble/src/main/java/com/like/ble/invoker/Invoker.kt
+++ ble/src/main/java/com/like/ble/invoker/Invoker.kt
@@ -87,7 +87,7 @@ class Invoker(private val mActivity: FragmentActivity) {
             }
 
             val curCommand = mCurCommand
-            // 判断需要抛弃
+            // 如果相同的命令正在执行，则抛弃
             if (curCommand != null &&
                 !curCommand.isCompleted() &&
                 curCommand::class.java == command::class.java &&
@@ -96,13 +96,13 @@ class Invoker(private val mActivity: FragmentActivity) {
                 Log.w(TAG, "命令正在执行，直接抛弃：$command")
                 return@launch
             }
-            // 判断立即执行
+            // 判断命令是否需要立即执行
             if (command is CloseCommand || command is StopScanCommand || command is DisconnectCommand || command is StopAdvertisingCommand) {
                 mCancel.set(true)
                 mCommands.send(command)
                 return@launch
             }
-            // 排队等候
+            // 排队等候前面的命令完成
             mCommands.send(command)
         }
     }
