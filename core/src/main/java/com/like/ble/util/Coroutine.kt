dd8239087baa1228531e4741f5dbafb934aa12ac
like5188 <260864866@qq.com>
2023-03-15 22:15:57 +0800

    完善

--- core/src/main/java/com/like/ble/util/Coroutine.kt
+++ core/src/main/java/com/like/ble/util/Coroutine.kt
@@ -7,6 +7,8 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withTimeout
 
 /**
+ * 用于把回调转换成带超时的挂起函数
+ *
  * @param timeout   超时时间。如果<=0，则表示不设置超时限制。和[withTimeout]方法的参数不一样。
  * @param errorMsg  超时抛的异常[TimeoutCancellationException]会被转换成[BleException]，这是指定的错误信息。
  */
@@ -16,6 +18,7 @@ suspend inline fun <T> suspendCancellableCoroutineWithTimeout(
 ): T = if (timeout > 0) {
     try {
         withTimeout(timeout) {
+            // 这里不能使用 suspendCoroutine，会因为它的阻塞并且不能取消，导致 withTimeout 不能按时触发，只能在它阻塞完成后才能触发。
             suspendCancellableCoroutine(block = block)
         }
     } catch (e: TimeoutCancellationException) {
