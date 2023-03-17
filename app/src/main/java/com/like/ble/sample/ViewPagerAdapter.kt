aa01098aea3808b1f28cb64ebe161c14a0fb1ca8
like <260864866@qq.com>
2023-03-17 11:18:00 +0800

    完善

--- app/src/main/java/com/like/ble/sample/ViewPagerAdapter.kt
+++ app/src/main/java/com/like/ble/sample/ViewPagerAdapter.kt
@@ -14,4 +14,15 @@ class ViewPagerAdapter(private val fragments: List<Fragment>, fragmentActivity: 
         return fragments[position]
     }
 
+    /**
+     * 默认实现适用于不添加、移动和移除项的集合。因为默认使用 position 来作为 itemId，这在添加删除时，会出错的，因为 position 时随添加删除而变化的
+     */
+    override fun getItemId(position: Int): Long {
+        return if (position == 0) {// 扫描界面
+            0
+        } else {// 连接界面
+            (fragments[position] as BleConnectFragment).getBleScanInfo().hashCode().toLong()
+        }
+    }
+
 }
