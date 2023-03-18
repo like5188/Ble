e42a61ae84fd1b2524f9a438094f159c53549139
like <260864866@qq.com>
2023-03-18 15:23:07 +0800

    完善

--- core/src/main/java/com/like/ble/util/scanrecordcompat/ScanRecordBelow21.java
+++ core/src/main/java/com/like/ble/util/scanrecordcompat/ScanRecordBelow21.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import android.os.ParcelUuid;
-import android.util.ArrayMap;
 import android.util.Log;
 import android.util.SparseArray;
 
