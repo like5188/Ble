fedba6e3085c152ccebbada8ca5d993ef4c5fd47
like <260864866@qq.com>
2023-03-27 11:04:55 +0800

    完善

--- central/src/main/java/com/like/ble/central/connect/executor/AbstractConnectExecutor.kt
+++ central/src/main/java/com/like/ble/central/connect/executor/AbstractConnectExecutor.kt
@@ -28,16 +28,19 @@ abstract class AbstractConnectExecutor(activity: ComponentActivity, val address:
 
     /**
      * 连接蓝牙设备
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun connect(timeout: Long = 10000L): List<BluetoothGattService>
 
     /**
      * 扫描并连接蓝牙设备
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun scanAndConnect(timeout: Long = 10000L): List<BluetoothGattService>
 
     /**
      * 断开蓝牙设备
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract fun disconnect()
 
@@ -47,6 +50,7 @@ abstract class AbstractConnectExecutor(activity: ComponentActivity, val address:
      * @param characteristicUuid        特征UUID
      * @param serviceUuid               服务UUID，如果不为null，则会在此服务下查找[characteristicUuid]；如果为null，则会遍历所有服务查找第一个匹配的[characteristicUuid]
      * @return 特征值
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun readCharacteristic(
         characteristicUuid: UUID,
@@ -60,6 +64,8 @@ abstract class AbstractConnectExecutor(activity: ComponentActivity, val address:
      * @param descriptorUuid            描述UUID，属于[characteristicUuid]
      * @param characteristicUuid        特征UUID，如果不为null，则会在此特征下查找[descriptorUuid]；如果为null，则会遍历所有特征查找第一个匹配的[descriptorUuid]
      * @param serviceUuid               服务UUID，如果不为null，则会在此服务下查找[characteristicUuid]；如果为null，则会遍历所有服务查找第一个匹配的[characteristicUuid]
+     *
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun readDescriptor(
         descriptorUuid: UUID,
@@ -69,7 +75,7 @@ abstract class AbstractConnectExecutor(activity: ComponentActivity, val address:
     ): ByteArray
 
     /**
-     * 失败返回-1
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun readRemoteRssi(timeout: Long = 3000L): Int
 
@@ -77,11 +83,13 @@ abstract class AbstractConnectExecutor(activity: ComponentActivity, val address:
      * 快速传输大量数据时设置[android.bluetooth.BluetoothGatt.CONNECTION_PRIORITY_HIGH]，完成后要设置成默认的: [android.bluetooth.BluetoothGatt.CONNECTION_PRIORITY_BALANCED]
      *
      * @param connectionPriority    需要设置的priority。[android.bluetooth.BluetoothGatt.CONNECTION_PRIORITY_BALANCED]、[android.bluetooth.BluetoothGatt.CONNECTION_PRIORITY_HIGH]、[android.bluetooth.BluetoothGatt.CONNECTION_PRIORITY_LOW_POWER]
+     *
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun requestConnectionPriority(@IntRange(from = 0, to = 2) connectionPriority: Int, timeout: Long = 3000L)
 
     /**
-     * 失败返回-1
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun requestMtu(@IntRange(from = 23, to = 517) mtu: Int, timeout: Long = 3000L): Int
 
@@ -93,6 +101,7 @@ abstract class AbstractConnectExecutor(activity: ComponentActivity, val address:
      * @param serviceUuid                   服务UUID，如果不为null，则会在此服务下查找[characteristicUuid]；如果为null，则会遍历所有服务查找第一个匹配的[characteristicUuid]
      * @param type                          类型：0 (notification 不需要应答)；1 (indication 需要客户端应答)
      * @param enable                        true：开启；false：关闭
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun setCharacteristicNotification(
         characteristicUuid: UUID,
@@ -115,6 +124,7 @@ abstract class AbstractConnectExecutor(activity: ComponentActivity, val address:
      * WRITE_TYPE_DEFAULT 默认类型，需要外围设备的确认，也就是需要外围设备的回应，这样才能继续发送写。
      * WRITE_TYPE_NO_RESPONSE 设置该类型不需要外围设备的回应，可以继续写数据。加快传输速率。
      * WRITE_TYPE_SIGNED 写特征携带认证签名，具体作用不太清楚。
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun writeCharacteristic(
         data: ByteArray,
@@ -133,6 +143,8 @@ abstract class AbstractConnectExecutor(activity: ComponentActivity, val address:
      * @param descriptorUuid            描述UUID，属于[characteristicUuid]
      * @param characteristicUuid        特征UUID，如果不为null，则会在此特征下查找[descriptorUuid]；如果为null，则会遍历所有特征查找第一个匹配的[descriptorUuid]
      * @param serviceUuid               服务UUID，如果不为null，则会在此服务下查找[characteristicUuid]；如果为null，则会遍历所有服务查找第一个匹配的[characteristicUuid]
+     *
+     * @throws [com.like.ble.exception.BleException]
      */
     abstract suspend fun writeDescriptor(
         data: ByteArray,
