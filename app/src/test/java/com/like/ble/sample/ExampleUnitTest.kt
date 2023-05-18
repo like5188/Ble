235cc03ba2744e59f25a54c751acc187840aeb0d
like <260864866@qq.com>
2023-05-18 15:54:25 +0800

    完善注释

--- app/src/test/java/com/like/ble/sample/ExampleUnitTest.kt
+++ app/src/test/java/com/like/ble/sample/ExampleUnitTest.kt
@@ -1,8 +1,15 @@
 package com.like.ble.sample
 
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
 import org.junit.Test
-
-import org.junit.Assert.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +17,37 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    var cancellableContinuation: CancellableContinuation<*>? = null
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun addition_isCorrect() = runBlocking {
+        channelFlow<Unit> {
+            try {
+                suspendCancellableCoroutine<Unit> {
+                    cancellableContinuation = it
+                    it.invokeOnCancellation {
+                        println("invokeOnCancellation")
+                        close()
+                    }
+                    onStartScan(it)
+                    println("onStartScan")
+                }
+                delay(5000)
+            } catch (e: Exception) {
+                cancellableContinuation?.cancel()
+            }
+        }.catch {
+            println(it)
+        }.collect()
+        println("finish")
+    }
+
+    private fun onStartScan(continuation: CancellableContinuation<Unit>) {
+        continuation.cancel()
+        println("0")
+//        continuation.resume(Unit)
+        println("1")
+//        continuation.resumeWithException(IllegalArgumentException("err"))
+        println("2")
     }
 }
