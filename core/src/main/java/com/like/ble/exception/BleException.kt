8111b02fbecbdd527d5c63c0963cf4a796f67a98
like <260864866@qq.com>
2023-07-18 10:21:33 +0800

    完善

--- core/src/main/java/com/like/ble/exception/BleException.kt
+++ core/src/main/java/com/like/ble/exception/BleException.kt
@@ -7,6 +7,8 @@ class BleExceptionDeviceDisconnected(address: String?) : BleException("蓝牙设
 class BleExceptionDiscoverServices(address: String?) : BleException("发现服务失败:$address", -5)
 class BleExceptionBusy(message: String? = "蓝牙繁忙，请稍后再试！") : BleException(message, -6)
 class BleExceptionTimeout(message: String?) : BleException(message, -7)
+
+// 提前取消超时不做处理。因为这是调用 stopScan() 或者 disconnect() 方法造成的，使用者可以直接在 stopScan() 或者 disconnect() 方法结束后处理 UI 的显示，不需要此回调。
 object BleExceptionCancelTimeout : BleException("操作被取消", -8)
 
 fun Exception.toBleException(): BleException {
