ccfc5ee0d38beaf89254722ba8242429ba1fdd46
like <260864866@qq.com>
2023-07-18 10:11:52 +0800

    完善

--- central/src/main/java/com/like/ble/central/scan/executor/AbstractScanExecutor.kt
+++ central/src/main/java/com/like/ble/central/scan/executor/AbstractScanExecutor.kt
@@ -2,6 +2,7 @@ package com.like.ble.central.scan.executor
 
 import android.content.Context
 import com.like.ble.central.scan.result.ScanResult
+import com.like.ble.exception.BleExceptionTimeout
 import com.like.ble.executor.BleExecutor
 import kotlinx.coroutines.flow.Flow
 
@@ -16,6 +17,7 @@ abstract class AbstractScanExecutor(context: Context) : BleExecutor(context) {
      * android 7.0 后不能在30秒内扫描和停止超过5次。android 蓝牙模块会打印当前应用扫描太频繁的log日志,并在android 5.0 的ScanCallback回调中触发onScanFailed(int）,返回错误码：ScanCallback.SCAN_FAILED_APPLICATION_REGISTRATION_FAILED,表示app无法注册，无法开始扫描）。
      *
      * @param timeout               扫描持续时长。达到时长后，会自动停止扫描；如果<=0，表示不限制。
+     * 当抛出异常[BleExceptionTimeout]代表扫描完成了，即[timeout]到了。
      */
     abstract fun startScan(timeout: Long = 10000L): Flow<ScanResult>
 
