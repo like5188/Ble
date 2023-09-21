9aa50c54ab9f3c53f4aa0a416072712ac2626e29
like <260864866@qq.com>
2023-09-21 09:53:39 +0800

    解决锁占用提示混乱问题

--- core/src/main/java/com/like/ble/util/MutexUtils.kt
+++ core/src/main/java/com/like/ble/util/MutexUtils.kt
@@ -14,9 +14,14 @@ class MutexUtils {
     val mutex = Mutex()
 
     /**
+     * 当前占用锁的提示信息
+     */
+    var cur: String? = null
+
+    /**
      * 当锁被占用了，就抛出[BleExceptionBusy]异常，没被占用，就执行[action]代码块
      *
-     * @param busyMessage   繁忙异常的提示信息
+     * @param busyMessage   如果当前已经占用，那么其它操作来获取锁，就会提示这个信息
      * @param action        需要执行的代码块
      */
     @OptIn(ExperimentalContracts::class)
@@ -25,8 +30,9 @@ class MutexUtils {
             callsInPlace(action, InvocationKind.EXACTLY_ONCE)
         }
 
-        if (!mutex.tryLock()) throw BleExceptionBusy(busyMessage)
+        if (!mutex.tryLock()) throw BleExceptionBusy(cur)
         try {
+            cur = busyMessage
             return action()
         } finally {
             mutex.unlock()
