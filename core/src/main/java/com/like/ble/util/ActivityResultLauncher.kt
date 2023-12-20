9af904215d44dd9a2b27f52ead97d6f3f702912c
like <260864866@qq.com>
2023-12-20 15:18:47 +0800

    完善文档

--- core/src/main/java/com/like/ble/util/ActivityResultLauncher.kt
+++ core/src/main/java/com/like/ble/util/ActivityResultLauncher.kt
@@ -27,6 +27,10 @@ open class BaseActivityResultLauncher<I, O>(
     fun launch(input: I, options: ActivityOptionsCompat? = null, callback: ActivityResultCallback<O>) {
         val key = "com.like.ble.util.BaseActivityResultLauncher.${hashCode()}"
         var launcher: ActivityResultLauncher<I>? = null
+        // 除了带有 LifecycleOwner 参数的 register 函数（此函数如果使用位置不当，会报错：LifecycleOwners must call register before they are STARTED）外，
+        // 还有一个不带 LifecycleOwner 的函数，这里就使用了它，它内部没有对生命周期相关的判断，
+        // 所以需要我们自己管理 unregister 的调用，这里在返回结果时调用 unregister。
+        // 所以也就不支持在 Launcher 的重复使用，每次都会去创建一个新的 Launcher。
         launcher = registry.register(key, contract) { result ->
             callback.onActivityResult(result)
             launcher?.unregister()
